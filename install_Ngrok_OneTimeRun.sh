30ffb33d0b48972c66fc9409cf6642abf5139d0c
mohmaedabubaker09 <mohamedabubaker09@gmail.com>
2023-11-14 22:05:43 +0200

    Updated and added bash scripts to download, setup, and configure NGROK, and also export environment variables and run NGROK service

---
+++ install_Ngrok_OneTimeRun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+ sudo curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+   sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
+   echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
+   sudo tee /etc/apt/sources.list.d/ngrok.list && \
+   sudo apt update && sudo apt install -y ngrok
+
+
+ngrok config add-authtoken <TOKEN>
---
+++ start_services.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Environment Variables setup
+export BUCKET_NAME=""
+export S3_ACCESS_KEY=""
+export S3_SECRET_KEY=""
+export TELEGRAM_TOKEN=""
+export REGION=""
+
+ngrok http --domain=conversely-epic-glider.ngrok-free.app 8443
