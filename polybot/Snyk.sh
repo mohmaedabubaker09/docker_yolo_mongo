806022f7dd76525711e722432aca84e8c61f316f
mohmaedabubaker09 <mohamedabubaker09@gmail.com>
2023-11-16 19:26:11 +0200

    We added scripts to run synk and also added the synk scan result logs

---
+++ polybot/Snyk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+snyk container test mohamedabubaker09/lana_bot:ubuntu-jammy-20231004 --file=Dockerfile > ~/PycharmProjects/docker_yolo_mongo/polybot/snyk-scan-results/lana_bot:ubuntu-jammy-20231004_before.txt
---
+++ yolo5/Snyk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+snyk container test mohamedabubaker09/lana_yolo5:ultralytics-yolov5-latest-cpu --file=Dockerfile > ~/PycharmProjects/docker_yolo_mongo/yolo5/snyk-scan-results/lana_yolo5-ultralytics-yolov5-latest-cpu_before.txt
+
