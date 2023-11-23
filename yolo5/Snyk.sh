1473b81f754532e136a734e604c844d4086a267c
mohmaedabubaker09 <mohamedabubaker09@gmail.com>
2023-11-23 12:15:42 +0200

    Implemented multi-stage build in new Dockerfile2 for lanabot to reduce image size. Also established non-root users in both lanabot and lanayolo5 Docker containers for enhanced security.

--- yolo5/Snyk.sh
+++ yolo5/Snyk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# snyk container test mohamedabubaker09/lana_yolo5:ultralytics-yolov5-latest-cpu --file=Dockerfile
+
 snyk container test mohamedabubaker09/lana_yolo5:ultralytics-yolov5-latest-cpu --file=Dockerfile > ~/PycharmProjects/docker_yolo_mongo/yolo5/snyk-scan-results/lana_yolo5-ultralytics-yolov5-latest-cpu_before.txt
 
